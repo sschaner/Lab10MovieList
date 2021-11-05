38b71160dfde49bb0c236b83c64d38c19d7fe489
Steve Schaner <schanerst@gmail.com>
2021-11-04 21:48:26 -0400

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10MovieList
+{
+    class Movie
+    {
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string category;
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        public Movie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab10MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
